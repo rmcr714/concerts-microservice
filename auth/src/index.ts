32d9fbe81e318c89c15ab70388ddc4ae25d6cd40
unknown <tiwari136@gmail.com>
2021-05-24 23:43:05 +0530

    Project initiation with auth service with error handling mechanism

---
+++ auth/src/index.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import 'express-async-errors'
+import mongoose from 'mongoose'
+import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
+const app = express()
+
+app.use(json())
+
+app.use(currentUserRouter)
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('conected to mongodb')
+  } catch (err) {
+    console.log(err)
+  }
+  app.listen(3000, () => {
+    console.log('Auth listeining on port 3000')
+  })
+}
+
+start()
