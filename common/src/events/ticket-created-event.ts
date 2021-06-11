56658211388147ffe5a8d93505e20ece5cec2a03
unknown <tiwari136@gmail.com>
2021-06-11 18:56:21 +0530

    updates

---
+++ common/src/events/ticket-created-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './subjects'
+
+export interface ticketCreatedEvent {
+  subject: Subjects.TicketCreated
+  data: {
+    id: string
+    title: string
+    price: number
+    userId: string
+  }
+}
